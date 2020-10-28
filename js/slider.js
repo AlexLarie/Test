5e2141b87fd3f88c694e5b99945ec909006e4516
AlexLarie <leshabelov@mail.ru>
2020-10-29 02:01:44 +0300

    added files and do till partners inclood

---
+++ js/slider.js
@@ -0,0 +1,17 @@
+$('#multiCarousel').carousel({
+    interval: 10000
+})
+$('.comments__carousel .comments__item').each(function(){
+    var next = $(this).next();
+    if (!next.length) {
+    next = $(this).siblings(':first');
+    }
+    next.children(':first-child').clone().appendTo($(this));
+    for (var i=0;i<2;i++) {
+        next=next.next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+    }
+});
